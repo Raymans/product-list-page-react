b1006cd639ba2d7a0ef551b449300eff1a2c67af
Raymans Peng <rpeng@digitalriver.com>
2017-03-23 17:35:14 +0800

    Init with table components.

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import './reactAtlas.min.css';
+import {Table, TableHeaderColumn} from 'react-atlas'
+
+const products = [];
+
+function addProducts(quantity) {
+    const startId = products.length;
+    for (let i = 0; i < quantity; i++) {
+        const id = startId + i;
+        products.push({
+            id: id,
+            name: 'Item name raymans ' + id,
+            price: 2100 + i
+        });
+    }
+}
+
+addProducts(70);
+
+const selectRowProp = {
+    mode: 'checkbox'
+};
+
+
+class App extends Component {
+    
+  render() {
+    this.options = {
+        defaultSortName: 'name',  // default sort column name
+        defaultSortOrder: 'desc'  // default sort order
+    };
+    return (
+        <Table data={ products } selectRow={ selectRowProp } options={this.options} >
+            <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='name' dataSort={ true }>Product Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        </Table>
+    );
+  }
+}
+
+export default App;
